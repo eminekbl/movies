5255f7d38ac4efa75d3b033cc9547f9f14cfe2ba
Emine Kebeli <kbl.emine@gmail.com>
2021-06-27 17:50:18 +0300

    fix updateitemdto

--- Dtos/UpdateItemDto.cs
+++ Dtos/UpdateItemDto.cs
@@ -9,6 +9,7 @@ namespace movie.Dtos
         public string Description { get; init; }
         public string Year { get; init; }
         public string Poster { get; init; }
+        public string Trailer { get; init; }
         public string Category { get; init; }
 
 
